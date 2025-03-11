4e65879f97751ae6da0347b80ce10b45b83720a2
Xianyx <pushkar44320@gmail.com>
2025-03-11 21:30:31 +0530

    aceternity.ui integration ??

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap(), react()],
+  integrations: [mdx(), sitemap(), react(),],
 
   vite: {
     plugins: [tailwindcss()],
